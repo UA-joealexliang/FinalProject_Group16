6a61309a7660fcd95a476ad6bb73e1674112adbb
rahelmiz <rahlboo11@gmail.com>
2021-11-11 17:45:13 -0700

    hello! pushing

--- src/Budget.java
+++ src/Budget.java
@@ -3,4 +3,7 @@ import java.util.Collections;
 
 public class Budget {
 	public ArrayList<Category> Categories = new ArrayList<Category>();
+	public double total_income; 
+	public double unassigned_balance;
+	public double total_assigned;
 }
