524cabbdefc692a7712461eca611685b68349ce8
rahelmiz <rahlboo11@gmail.com>
2021-11-11 18:24:00 -0700

    hello

--- src/Budget.java
+++ src/Budget.java
@@ -6,4 +6,15 @@ public class Budget {
 	public double total_income; 
 	public double unassigned_balance;
 	public double total_assigned;
+	
+	public double get_total_balance( ) {
+		return 2;
+	}
+	public double add_category(String name, Double amount ) {
+		return 2;
+	}
+	public void add_subcategory(String parent, String name, double amount) {
+		return;
+	}
+	
 }
