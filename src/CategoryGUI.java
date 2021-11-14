fb078f6844137a9323ce690752e36bea2f0575ec
MichelleLoustaunau <94149543+MichelleLoustaunau@users.noreply.github.com>
2021-11-13 17:42:28 -0700

    GUI's
    Added to all GUI's

--- src/CategoryGUI.java
+++ src/CategoryGUI.java
@@ -3,6 +3,35 @@ import java.awt.*;
 import java.awt.event.*;
 
 import javax.swing.*;
-public class CategoryGUI {
-
+public class CategoryGUI extends JFrame{
+	//declare the private variables
+		private JPanel contentPane;
+		
+		public static void main(String[] args) {
+			EventQueue.invokeLater(new Runnable() {
+				public void run() {
+					//try catch block
+					try {
+						//create object CategoryGUI
+						CategoryGUI frame = new CategoryGUI(); 
+						
+					}
+					catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			});
+		}
+	public CategoryGUI() {
+		setTitle("New Category");//set frame title
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);//set default operation
+		setBounds(100, 100, 450, 300); //sets bounds of frame
+		contentPane = new JPanel();//creates object pannel
+		contentPane.setBorder(new EmptyBorder(5,5,5,5));//sets border
+		setContentPane(contentPane); //set contentpane
+		contentPane.setLayout(null); //set null
+		
+		
+		
+	}
 }
--- src/MainGUI.java
+++ src/MainGUI.java
@@ -26,12 +26,25 @@ public class MainGUI extends JFrame {
 	//create frame
 	public MainGUI() {//constructor
 		setTitle("Budget");//set frame title
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//set default operation
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);//set default operation
 		setBounds(100, 100, 450, 300); //sets bounds of frame
 		contentPane = new JPanel();//creates object pannel
 		contentPane.setBorder(new EmptyBorder(5,5,5,5));//sets border
 		setContentPane(contentPane); //set contentpane
 		contentPane.setLayout(null); //set null
-		JButton AddTransaction = new JButton("Add Transaction");
+		JButton AddTransaction = new JButton("Add Transaction");//setting transaction button 
+		AddTransaction.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				TransactionGUI frame = new TransactionGUI();//calls object TransactionGUI and sets it visible to true
+				frame.setVisible(true);
+				setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); //set default close operation
+			}
+		});
+		
+		
+		//add buttons to contentPane
+		contentPane.add(AddTransaction);//transaction button
+		//add components to the frame 
+		frame.getContentPane().add(BorderLayout.SOUTH, contentPane);//sets button to the bottom
 	}
 }
